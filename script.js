0bb4ab57f09d391383ee0f8cf04b96520e47852f
Kelvin-tec <simiyukelvin22@gmail.com>
2024-10-27 14:09:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function mymenuFunction(){
+    var menubtn = document.getElementById("mynavmenu");
+    if(menubtn.className==="nav-menu"){
+        menubtn.className += " responsive ";
+    } else{
+        menubtn.className = "nav-menu";
+    }
+}
+
+window.onscroll=function(){headershadow()};
+function headershadow(){
+    const navHeader=document.getElementById("header");
+
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+        navHeader.style.boxShadow= "0 1px 6px rgba(0,0,0,0.1)";
+        navHeader.style.height="70px";
+        navHeader.style.lineHeight="70px";
+    }else{
+        navHeader.style.boxShadow= "none";
+        navHeader.style.height="90px";
+        navHeader.style.lineHeight="90px" ;
+    }
+}
